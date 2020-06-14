0304300828efc154312120228fd7773d68bb474c
Vivianas9244 <49568184+Vivianas9244@users.noreply.github.com>
2020-06-14 19:40:32 -0400

    Update: README.md notebooks

---
+++ notebooks/Shared/HeartDisease.scala
@@ -0,0 +1,19 @@
+// Databricks notebook source
+// MAGIC %fs ls
+
+// COMMAND ----------
+
+// MAGIC %fs ls dbfs:/FileStore/tables
+
+// COMMAND ----------
+
+// MAGIC %scala 
+// MAGIC val hd = spark.read.format("csv")
+// MAGIC .option("header", "true")
+// MAGIC .option("inferSchema", "true")
+// MAGIC .load("/FileStore/tables/heartdisease_csv.csv")
+// MAGIC 
+// MAGIC display(hd)
+
+// COMMAND ----------
+
